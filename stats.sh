f277c1c4dc9b2d216fc01fa6569f865a000a6a31
ivan.pohosov <pogosovivan@gmail.com>
2025-01-10 01:33:46 +0200

    beautify script

--- stats.sh
+++ stats.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 DIR="./data/accounts"
-echo -e "Email\tTotal_Earnings"
+
+# Print the header with nice formatting
+echo -e "Email\t\t\t\tTotal_Earnings"
+echo -e "-----------------------------------------------------"
+
+# Collect data to create a structured format for column
+output=""
 
 for file in "$DIR"/*; do
     if [[ -f $file ]]; then
         email=$(awk -F= '/^Email=/{print $2}' "$file")
         earnings=$(awk -F= '/^Total_Earnings=/{print $2}' "$file")
         if [[ -n $email && -n $earnings ]]; then
-            echo -e "$email\t$earnings"
+            output+="$email\t$earnings\n"
         fi
     fi
 done
+
+# Use column to format the output
+echo -e "$output" | column -t -s $'\t'
